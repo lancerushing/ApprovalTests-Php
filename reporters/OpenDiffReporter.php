ce5ca8b72152254a7dcfc4cc0ef8b9466b1c8ed0
Lance Rushing <lancerushing@gmail.com>
2012-04-30 21:06:58 -0700

    Added path to require_once so requires work even w/o include_path being set

--- reporters/OpenDiffReporter.php
+++ reporters/OpenDiffReporter.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'Reporter.php';
-require_once 'FileUtil.php';
+require_once dirname(__DIR__) . '/FileUtil.php';
 
 class OpenDiffReporter implements Reporter {
 	public function reportFailure($approvedFilename, $receivedFilename) {
